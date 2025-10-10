9496cd0f81841c2ecce7652a7d8473e68d6e4f3f
Pedro Guia <u202212010@upc.edu.pe>
2025-10-10 08:31:07 -0500

    feat(IAM): changed.

--- AidManager.API/IAM/Domain/Repositories/IGoogleAuthorization.cs
+++ AidManager.API/IAM/Domain/Repositories/IGoogleAuthorization.cs
@@ -5,6 +5,6 @@ namespace AidManager.API.Shared.Domain.Repositories;
 public interface IGoogleAuthorization
 {
     string GetAuthorizationUrl();
-    Task<UserCredential> ExchangeCodeFforToken(string code);
+    Task<UserCredential> ExchangeCodeForToken(string code);
     Task<UserCredential> ValidateToken(string accessToken);
 }
