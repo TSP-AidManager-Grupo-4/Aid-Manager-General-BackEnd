852a62e67d05cf17bf102352e22e26d8d296c440
Pedro Guia <u202212010@upc.edu.pe>
2025-10-10 10:13:38 -0500

    feat(IAM): add controller Authorize.

---
+++ AidManager.API/IAM/Interfaces/AuthorizeController.cs
@@ -0,0 +1,41 @@
+using System.Text.Json;
+using AidManager.API.Shared.Domain.Entities;
+using AidManager.API.Shared.Domain.Repositories;
+using AidManager.API.Shared.Infraestructure.Persistence.EFC.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AidManager.API.IAM.Interfaces.REST;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorizeController(IGoogleAuthorization googleAuthorization, AppDBContext context):
+    ControllerBase
+{
+    [HttpGet]
+    public IActionResult Authorize() => Ok(googleAuthorization.GetAuthorizationUrl());
+
+    [HttpGet("callback")]
+    public async Task<IActionResult> Callback(string code)
+    {
+        var userCredential = await googleAuthorization.ExchangeCodeForToken(code);
+        var _credential = await context.Credentials
+            .FirstOrDefaultAsync(c=>c.AccessToken == userCredential.Token.AccessToken);
+        return Redirect("$");
+    }
+
+    [HttpGet("token/{userId]")]
+    public async Task<IActionResult> GetAccessToken(string userId)
+    {
+        Guid _userId = Guid.Empty;
+        try
+        {
+            _userId = Guid.Parse(userId);
+        }
+        catch { return Unauthorized(); }
+
+        var credential = await context.Credentials.FirstOrDefaultAsync(c => c.UserId == _userId);
+        return Ok(JsonSerializer.Serialize
+            (new Token(credential.AccessToken, credential.UserId.ToString())));
+    }
+}
